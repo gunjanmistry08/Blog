bad970d75a5d1fa972f22001d1010058f480609a
gunjan mistry <gunjan.mistry@somaiya.edu>
2020-03-20 12:11:22 +0530

    user login system created

--- django_project/urls.py
+++ django_project/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('register/',user_views.register, name='register'),
+    path('profile/',user_views.profile, name='profile'),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegistrationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def register(request):
@@ -14,3 +15,7 @@ def register(request):
     else:
         form = UserRegistrationForm()
     return render(request,'users/register.html',{'form':form})
+
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
