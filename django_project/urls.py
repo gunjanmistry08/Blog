8d00d6c2d8df2c00afe5693d1162386bb63e85fc
gunjan mistry <gunjan.mistry@somaiya.edu>
2020-03-21 14:23:05 +0530

    pwd reset half done

--- django_project/urls.py
+++ django_project/urls.py
@@ -27,6 +27,9 @@ urlpatterns = [
     path('profile/',user_views.profile, name='profile'),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('password-reset/',auth_views.PasswordResetView.as_view(template_name='users/password-reset.html'), name='password-reset'),
+    path('password-reset/done',auth_views.PasswordResetDoneView.as_view(template_name='users/password-reset-done.html'),name='password-reset-done'),
+    path('password-reset-confirm/<uidb64>/<token>',auth_views.PasswordResetConfirmView.as_view(template_name='users/password-reset-confirm.html'), name='password-reset-confirm'),
 ]
 
 if settings.DEBUG:
