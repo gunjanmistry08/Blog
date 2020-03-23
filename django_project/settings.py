ba562b518a757102fc2221ffe203f5c25d74a4e5
gunjan mistry <gunjan.mistry@somaiya.edu>
2020-03-23 13:58:24 +0530

    Password Reset Module Done

--- django_project/settings.py
+++ django_project/settings.py
@@ -131,3 +131,11 @@ LOGIN_URL = 'login' #get to where you wanna go after you login next?=
 MEDIA_ROOT =  os.path.join(BASE_DIR,'media')
 
 MEDIA_URL = '/media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'gunjan.mistry@somaiya.edu'
+EMAIL_HOST_PASSWORD = '008gunjan'
