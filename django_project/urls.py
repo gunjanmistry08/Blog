d9be2a19aa79c0791ab59b4dd004d5a30db38eed
gunjan mistry <gunjan.mistry@somaiya.edu>
2020-03-23 15:11:22 +0530

    resolved merge conflict

--- django_project/urls.py
+++ django_project/urls.py
@@ -27,16 +27,9 @@ urlpatterns = [
     path('profile/',user_views.profile, name='profile'),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-<<<<<<< HEAD
-    path('password-reset/',auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),name='password_reset'),
-    path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),name='password_reset_confirm'),
-    path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),name='password_reset_complete'),
-=======
     path('password-reset/',auth_views.PasswordResetView.as_view(template_name='users/password-reset.html'), name='password-reset'),
     path('password-reset/done',auth_views.PasswordResetDoneView.as_view(template_name='users/password-reset-done.html'),name='password-reset-done'),
     path('password-reset-confirm/<uidb64>/<token>',auth_views.PasswordResetConfirmView.as_view(template_name='users/password-reset-confirm.html'), name='password-reset-confirm'),
->>>>>>> 8d00d6c2d8df2c00afe5693d1162386bb63e85fc
 ]
 
 if settings.DEBUG:
